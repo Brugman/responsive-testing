a66d0b1b1bc9bc1819d0a750e3dd8e156307d8e1
Tim Brugman <tim@timbrugman.com>
2019-02-11 17:51:35 +0100

    Removed livereload.

--- app/core-foot.php
+++ app/core-foot.php
@@ -1,6 +1,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="/assets/rt.min.js"></script>
-<script src="http://<?=$_SERVER['HTTP_HOST'];?>:35729/livereload.js"></script>
 
 </body>
 </html>
