5941263130704973356c0575d5dfa4cb45058a66
Tim Brugman <tim@timbrugman.com>
2021-08-29 00:33:29 +0200

    Add Bootstrap 5 preset
    Close #17

--- app/config-example.php
+++ app/config-example.php
@@ -22,5 +22,17 @@ $presets = [
             '1200' => 'xl - extra large',
         ],
     ],
+    [
+        'key'       => 'bs-5',
+        'name'      => 'Bootstrap 5',
+        'viewports' => [
+            '320'  => 'xs - extra small',
+            '576'  => 'sm - small',
+            '768'  => 'md - medium',
+            '992'  => 'lg - large',
+            '1200' => 'xl - extra large',
+            '1400' => 'xxl - extra extra large',
+        ],
+    ],
 ];
 
